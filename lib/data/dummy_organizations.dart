cecccec2a5fab370354314adbb38e0e0ca1de7ee
Justin-Roy-Santos <jbsantos15@up.edu.ph>
2024-05-06 23:25:23 +0800

    chore: Create dummy organizations data

---
+++ lib/data/dummy_organizations.dart
@@ -0,0 +1,36 @@
+// data model
+import 'package:elbi_donation_system/data_models/organization.dart';
+
+// dummy list of organizations
+final List<Organization> dummyOrganization = [
+  Organization(
+    name: 'Pastor Agila',
+    username: 'pastor_agila',
+    password: '123456789',
+    addresses: ['Socorro, Surigao del Norte, Philippines'],
+    contactNumber: '0998765432',
+    organizationName: 'Socorro Bayanihan Services, Inc.',
+    proofsOfLegitimacy: [],
+    orgImagePath: 'assets\images\org_images\kulto.png',
+  ),
+  Organization(
+    name: 'Bob Jones',
+    username: 'earthIsFlat',
+    password: '123456789',
+    addresses: ['Florida, USA'],
+    contactNumber: '0998765432',
+    organizationName: 'Flat Earthers',
+    proofsOfLegitimacy: [],
+    orgImagePath: 'assets\images\org_images\isis.jpg',
+  ),
+  Organization(
+    name: 'Abdul Ahmed',
+    username: 'abdul_ahmed',
+    password: '123456789',
+    addresses: ['Somewhere in Iraq'],
+    contactNumber: '0912345678',
+    organizationName: 'ISIS',
+    proofsOfLegitimacy: [],
+    orgImagePath: 'assets\images\org_images\isis.jpg',
+  ),
+];
